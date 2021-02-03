33879a4af868fb7e661bc6899531320de15d086b
William Trépanier <wtrepanier@coveo.com>
2021-02-03 13:50:31 -0500

    added website

---
+++ docs/js/coveo.extension.js
@@ -0,0 +1,177 @@
+var CoveoExtension =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/Index.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/Index.ts":
+/*!**********************!*\
+  !*** ./src/Index.ts ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+// This entry point defines all the component that should be included in your extension.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HelloWorld = void 0;
+var HelloWorld_1 = __webpack_require__(/*! ./ui/HelloWorld */ "./src/ui/HelloWorld.ts");
+Object.defineProperty(exports, "HelloWorld", { enumerable: true, get: function () { return HelloWorld_1.HelloWorld; } });
+
+
+/***/ }),
+
+/***/ "./src/ui/HelloWorld.ts":
+/*!******************************!*\
+  !*** ./src/ui/HelloWorld.ts ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HelloWorld = void 0;
+var coveo_search_ui_1 = __webpack_require__(/*! coveo-search-ui */ "coveo-search-ui");
+var HelloWorld = /** @class */ (function (_super) {
+    __extends(HelloWorld, _super);
+    function HelloWorld(element, options, bindings) {
+        var _this = _super.call(this, element, HelloWorld.ID, bindings) || this;
+        _this.element = element;
+        _this.options = options;
+        _this.bindings = bindings;
+        _this.options = coveo_search_ui_1.ComponentOptions.initComponentOptions(element, HelloWorld, options);
+        coveo_search_ui_1.$$(_this.element).text(_this.options.dummyOptionText);
+        _this.bind.onRootElement(coveo_search_ui_1.QueryEvents.buildingQuery, function (args) { return _this.handleBuildingQuery(args); });
+        return _this;
+    }
+    HelloWorld.prototype.handleBuildingQuery = function (args) {
+        args.queryBuilder.advancedExpression.add(this.options.dummyOptionQuery);
+    };
+    HelloWorld.ID = 'HelloWorld';
+    HelloWorld.options = {
+        dummyOptionText: coveo_search_ui_1.ComponentOptions.buildStringOption({
+            defaultValue: 'Hello world'
+        }),
+        dummyOptionQuery: coveo_search_ui_1.ComponentOptions.buildStringOption({
+            defaultValue: '@uri'
+        })
+    };
+    return HelloWorld;
+}(coveo_search_ui_1.Component));
+exports.HelloWorld = HelloWorld;
+coveo_search_ui_1.Initialization.registerAutoCreateComponent(HelloWorld);
+
+
+/***/ }),
+
+/***/ "coveo-search-ui":
+/*!************************!*\
+  !*** external "Coveo" ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = Coveo;
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=coveo.extension.js.map
